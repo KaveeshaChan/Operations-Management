04cd42a9f7ab58b3e97ee3990dc92cc3a91694ca
Sandun D. Mendis <sandunmendis1998@gmail.com>
2025-02-25 12:06:50 +0530

    update select routs

--- backEnd/routes/selectRoutes.js
+++ backEnd/routes/selectRoutes.js
@@ -3,11 +3,13 @@ const { authorizeRoles } = require('./../middlewares/authMiddleware');
 const viewFreightAgentsRoute = require('../controllers/userHandlingControllers/viewFreightAgents');
 const viewOrdersToAgents = require('../controllers/orderHandlingControllers/viewOrdersToAgents')
 const viewQuotesForOrder = require('../controllers/orderHandlingControllers/exporterHandlings/viewQuotesForOrder')
+const viewMainUsers = require('../controllers/userHandlingControllers/viewMainUsers')
 
 const router = express.Router();
 
 router.use('/view-freight-agents', viewFreightAgentsRoute);
 router.use('/view-orders', viewOrdersToAgents);
 router.use('/view-quotes', viewQuotesForOrder);
+router.use('/view-mainUsers', viewMainUsers)
 
 module.exports = router;
